67d74c5013bbb9dc1a811a134eccc444cdac7653
dachui <dazhu.wang@trustbe.cn>
2022-12-11 22:48:50 +0800

    add test log

--- cpp_fence/test.cpp
+++ cpp_fence/test.cpp
@@ -1,34 +1,40 @@
+#include <cstdio>
 #include <deque>
+#include <memory>
+#include <mutex>
 #include <string>
 #include <thread>
-#include <mutex>
-#include <cstdio>
-#include <memory>
 
 std::mutex mtx;
-std::deque<std::string> test_queue;
+std::deque<std::string> queue;
 
-void thread_recv(){
+void thread_recv() {
     printf("thread_recv begin loop\n");
-    while (test_queue.empty()) {
-        //std::atomic_thread_fence(std::memory_order_acquire);  without this line, gcc-9, gcc-10 with -O1 will deal loop
+    while (queue.empty()) {
+        // without atomic_thread_fence test on platform amd64
+        //  compile -O0 -O1 -O2 -O3
+        //  g++-9    Y   N   N   N
+        //  g++-10   Y   N   Y   Y
+        //  g++-12   Y   N   Y   Y
+
+        // std::atomic_thread_fence(std::memory_order_consume);
     }
     printf("thread_recv end loop\n");
     mtx.lock();
-    auto str = std::move(test_queue.front());
-    test_queue.pop_front();
+    auto str = std::move(queue.front());
+    queue.pop_front();
     mtx.unlock();
 }
 
-void thread_send(){
+void thread_send() {
     std::this_thread::sleep_for(std::chrono::seconds(1));
     mtx.lock();
-    test_queue.emplace_back("can you out loop");
+    queue.emplace_back("can you out loop");
     mtx.unlock();
     printf("thread_send finished\n");
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char* argv[]) {
     std::thread t1(thread_recv);
     std::thread t2(thread_send);
     t1.join();
